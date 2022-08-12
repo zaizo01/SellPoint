129ab536505a037ffe02eab256c8d245c8e1cb3b
zaizo01 <e.zaizortega@gamil.com>
2022-08-12 01:12:58 -0400

    mapper config is done

---
+++ SellPoint.Presentation.WebAPI/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SellPoint.Data.DTOs.Entidades;
+using SellPoint.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellPoint.Presentation.WebAPI.MappingProfiles
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Entidades, EntidadesGetDTO>().ReverseMap();
+            CreateMap<Entidades, EntidadesPostDTO>().ReverseMap();
+        }
+    }
+}
